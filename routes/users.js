bb30bb64c600b821651654a9551a30fec4c00aed
edgarOPG <eopg95@gmail.com>
2017-03-09 00:32:09 -0700

    resolved router error

--- routes/users.js
+++ routes/users.js
@@ -3,22 +3,11 @@ var router = express.Router();
 
 /* GET users listing. */
 router.get('/', function(req, res, next) {
-  res.render('users/index', {});
-});
-
-router.get('/login', function(req, res, next) {
-  res.render('users/login', {});
+  res.render('users/blank', {});
 });
 
 router.get('/blank', (req, res, next) => {
   res.render('users/blank', {});
-});
-
-router.post('/create', (req, res, next) => {
-  console.log(req.body.usuario);
-  console.log(req.body.nombre);
-  console.log(req.body.primerApellido);
-  console.log(req.body.segundoApellido);
 });
 
 module.exports = router;
