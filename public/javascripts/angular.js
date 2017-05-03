feb922342093d8d249954d62a5c81c0d6569af6f
edgarOPG <eopg95@gmail.com>
2017-05-03 00:47:03 -0600

    added angularjs

--- public/javascripts/angular.js
+++ public/javascripts/angular.js
@@ -1,23 +1,7 @@
-function TodoCtrl($scope) {
-
-  $scope.todos = [
-    {text:'Learn AngularJS', done:false},
-    {text: 'Build an app', done:false}
-  ];
-
-  $scope.getTotalTodos = function () {
-    return $scope.todos.length;
-  };
-
-
-  $scope.addTodo = function () {
-    $scope.todos.push({text:$scope.formTodoText, done:false});
-    $scope.formTodoText = '';
-  };
-
-    $scope.clearCompleted = function () {
-        $scope.todos = _.filter($scope.todos, function(todo){
-            return !todo.done;
-        });
-    };
-}
+angular.module('todoAp', [])
+  .controller('ControladorTareas', ['$scope',
+function($scope) {
+  $scope.var = 'ASD';
+  $scope.tareas = [{texto: 'Ser Super Heroico con AngularJS', hecho: true},
+                   {texto: 'Crear una ap con angular', hecho: true}];
+}]);
