a3fdbc3c2486e3b82cf49b9e16162f4ab71ff753
edgarOPG <eopg95@gmail.com>
2017-05-03 01:23:29 -0600

    advances angularjs

--- public/javascripts/angular.js
+++ public/javascripts/angular.js
@@ -4,4 +4,10 @@ function($scope) {
   $scope.var = 'ASD';
   $scope.tareas = [{texto: 'Ser Super Heroico con AngularJS', hecho: true},
                    {texto: 'Crear una ap con angular', hecho: true}];
+
+  $scope.addSkill = function() {
+    console.log('asadsa')
+    $scope.tareas.push({texto: $scope.skillDescription, hecho: false});
+    $scope.skillDescription = '';
+  }
 }]);
