0700f87703f769f2c2d7d7c3521da535413cdeb0
edgarOPG <eopg95@gmail.com>
2017-05-06 01:19:00 -0600

    advances angularjs

--- public/javascripts/angular.js
+++ public/javascripts/angular.js
@@ -2,12 +2,12 @@ angular.module('todoAp', [])
   .controller('ControladorTareas', ['$scope',
 function($scope) {
   $scope.var = 'ASD';
-  $scope.tareas = [{texto: 'Ser Super Heroico con AngularJS', hecho: true},
-                   {texto: 'Crear una ap con angular', hecho: true}];
+  $scope.tareas = [{texto: 'Ser Super Heroico con AngularJS', ranking: 'Junior'},
+                   {texto: 'Crear una ap con angular', ranking: 'Master'}];
 
   $scope.addSkill = function() {
     console.log('asadsa')
-    $scope.tareas.push({texto: $scope.skillDescription, hecho: false});
+    $scope.tareas.push({texto: $scope.skillDescription, ranking: 'Junior'});
     $scope.skillDescription = '';
   }
 }]);
