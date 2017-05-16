35ac5b94fea4c630b3eb97a88c9da3ace23ae960
edgarOPG <eopg95@gmail.com>
2017-05-15 19:49:36 -0600

    refactor user edit

--- public/javascripts/angular.js
+++ public/javascripts/angular.js
@@ -1,10 +1,21 @@
 angular.module('todoAp', [])
   .controller('CtrlSkills', function($scope, $http) {
+
   $scope.skills = [];
 
+  $scope.rankings = [{value: 'Junior'},
+                    {value: 'Master'},
+                    {value: 'Senior'}];
+
   $scope.addSkill = function() {
-    $scope.skills.push({description: $scope.skillDescription, ranking: 'Junior'});
+    $scope.skills.push({description: $scope.skillDescription, ranking: $scope.rankingSelected.value});
     $scope.skillDescription = '';
+    $scope.postSkills();
+  }
+
+  $scope.removeSkill = function(index){
+    $scope.skills.splice(index, 1);
+    $scope.postSkills();
   }
 
   $scope.postSkills = function() {
