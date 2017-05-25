1f6f704c532cce4a010b2bcc4eb359cfa907a68f
gaqg <gaqg.13@hotmail.com>
2017-05-25 14:08:11 -0600

    test 3

--- app.js
+++ app.js
@@ -27,7 +27,7 @@ const projects = require('./app/routes/projects');
 const api = require('./app/routes/api');
 
 // configuration ===============================================================
-mongoose.connect(configFB.uri || configDB.url); // connect to our database
+mongoose.connect(configDB.uri || configDB.url); // connect to our database
 
 require('./config/passport')(passport); // pass passport for configuration
 
