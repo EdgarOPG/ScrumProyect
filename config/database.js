71457be38639ce4730919ddb11bec73dd132f8cb
gaqg <gaqg.13@hotmail.com>
2017-05-25 14:06:48 -0600

    test 3

--- config/database.js
+++ config/database.js
@@ -1,7 +1,7 @@
 // config/database.js
 module.exports = {
 
-    'url' : 'mongodb://localhost:27017/scrum_proyect' // looks like mongodb://<user>:<pass>@mongo.onmodulus.net:27017/Mikha4ot
+    'url' : 'mongodb://localhost:27017/scrum_proyect', // looks like mongodb://<user>:<pass>@mongo.onmodulus.net:27017/Mikha4ot
     'uri' : 'mongodb://gaspar:sdfdf2gg@ds151941.mlab.com:51941/heroku_ztdfvvfc'
 
 };
