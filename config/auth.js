89dedbd16a2df9a5864bcfa6b457e5344e522b05
gaqg <gaqg.13@hotmail.com>
2017-05-25 16:03:27 -0600

    test success

--- config/auth.js
+++ config/auth.js
@@ -6,7 +6,7 @@ module.exports = {
     'facebookAuth' : {
         'clientID'        : '2274321466126862', // your App ID
         'clientSecret'    : '22ae3d4cd3110f7aed94db30cbcfc472', // your App Secret
-        'callbackURL'     : 'http://scrumproject.herokuapp.com/auth/facebook/callback',
+        'callbackURL'     : 'http://localhost:8080/auth/facebook/callback',
         'profileURL': 'https://graph.facebook.com/v2.5/me?fields=first_name,last_name,email'
 
     },
@@ -14,13 +14,13 @@ module.exports = {
     'twitterAuth' : {
         'consumerKey'        : 'xKzQQ3YpY8foCNL4l2iwVkdhk',
         'consumerSecret'     : 'AAcHPimAA47FWinjQLWBczD7A51bH5glp5LJfIuW2fbwhOUgER',
-        'callbackURL'        : 'http://scrumproject.herokuapp.com/auth/twitter/callback'
+        'callbackURL'        : 'http://localhost:8080/auth/twitter/callback'
     },
 
     'googleAuth' : {
         'clientID'         : '51472290617-v3epql0sbqq9pol0eqevks1efhrgq0j0.apps.googleusercontent.com',
         'clientSecret'     : 'zb6mc-S2WqjHB4dYEuN1sPjg',
-        'callbackURL'      : 'http://scrumproject.herokuapp.com/auth/google/callback'
+        'callbackURL'      : 'http://localhost:8080/auth/google/callback'
     }
 
 };
